6ea136817e2097292893d8fd3161fd5a98851224
Luke Hanna <Luke.m.hanna@gmail.com>
2021-02-03 00:23:28 -0500

    Linked FXML to controller variables.

--- src/sample/loginController.java
+++ src/sample/loginController.java
@@ -1,5 +1,20 @@
 package sample;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class loginController
 {
+    public Button loginButton;
+    public Button registerButton;
+
+    public TextField usernameInput;
+    public PasswordField passwordInput;
+
+    public void login() {}
+
+    public void register() {}
+
+
 }
